4411c2bd8e8ba1560f4ba6e83bd7ec156b63345b
Wei Liu <liuweixa@redhat.com>
2024-01-26 17:44:31 +0800

    update device addon demo
    Signed-off-by: Wei Liu <liuweixa@redhat.com>

--- sensor-aggregator/demo.sh
+++ sensor-aggregator/demo.sh
@@ -10,18 +10,28 @@ cluster="edge"
 source ${demo_dir}/demo_magic
 
 export KUBECONFIG=${kubeconfig}
-comment "managed cluster and device addon on hub cluster"
+comment "An edge cluster is managed by ACM hub as a managed cluster"
 pe "kubectl get managedclusters"
+
+comment "A managedclusteraddon (Sensor Aggregator) is deployed on the edge cluster"
+comment "It is used to collect data from IoT devices/sensors"
 pe "kubectl -n ${cluster} get managedclusteraddons"
 
-comment "enable the opcua driver on the edge cluster"
-pe "kubectl -n ${cluster} apply -f ${demo_dir}/resources/opcua/driver.yaml"
-pe "kubectl -n ${cluster} get drivers opcua -oyaml"
+comment "A opcua server is running on the edge cluster to simulate opcua device"
+pe "kubectl -n opcua-server get svc"
 
-comment "enable a opcua device on the edge cluster"
+comment "Enable the opcua driver on the ACM hub"
+pe "kubectl -n ${cluster} apply -f ${demo_dir}/resources/opcua/driver.yaml"
+pe "kubectl -n ${cluster} get drivers.edge.open-cluster-management.io"
+
+comment "Add the simulated opcua device on the ACM hub"
 pe "kubectl -n ${cluster} apply -f ${demo_dir}/resources/opcua/device.yaml"
 pe "kubectl -n ${cluster} get devices opcua-s001 -oyaml"
 
+comment "The sensor aggregator has a build-in MQTT broker on the edge cluster by default"
+pe "kubectl -n open-cluster-management-agent-addon get svc"
+
 comment "You can receive the data of device opcua-s001 with MQTT topic devices/+/data/+ on tcp://127.0.0.1:1883"
+comment "e.g. mosquitto_sub -h 127.0.0.1 -p 1883 -t devices/+/data/+"
 
 unset KUBECONFIG
